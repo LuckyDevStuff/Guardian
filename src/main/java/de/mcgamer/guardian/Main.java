c2a4ec4654f78bbce859880334085ba092caaf51
LuckyDevStuff <69163739+LuckyDevStuff@users.noreply.github.com>
2021-01-01 18:11:13 +0100

    Setup
    Basic Project Setup

---
+++ src/main/java/de/mcgamer/guardian/Main.java
@@ -0,0 +1,20 @@
+package de.mcgamer.guardian;
+
+import de.luckydev.luckyms.MySQLDatabase;
+import de.luckydev.luckyms.MySQLService;
+import net.md_5.bungee.api.plugin.Plugin;
+
+public final class Main extends Plugin {
+
+    public static MySQLDatabase guardianDB;
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
